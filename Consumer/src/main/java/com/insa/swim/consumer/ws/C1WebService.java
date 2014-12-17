a4e5f467725d088eba1036a54cbb0a9e7a7619d5
Alex binguy <alex@binguy.com>
2014-12-17 14:39:43 +0100

    Consumer sends request to the provider through bus

--- Consumer/src/main/java/com/insa/swim/consumer/ws/C1WebService.java
+++ Consumer/src/main/java/com/insa/swim/consumer/ws/C1WebService.java
@@ -5,6 +5,7 @@
  */
 package com.insa.swim.consumer.ws;
 
+import compositeapp1.CompositeApp1Service1;
 import java.net.URL;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
@@ -25,6 +26,7 @@ import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.ws.WebServiceRef;
 
 /**
  *
@@ -32,113 +34,25 @@ import javax.xml.transform.stream.StreamResult;
  */
 @WebService(serviceName = "C1WebService")
 public class C1WebService {
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_9080/CompositeApp1Service1/casaPort1.wsdl")
+   private CompositeApp1Service1 service;
 
     @WebMethod(operationName = "sendPing")
     public String sendPing(@WebParam(name = "start") String txt) {
 
-        try {
-            SOAPConnectionFactory sfc = SOAPConnectionFactory.newInstance();
-            SOAPConnection connection = sfc.createConnection();
-
-            MessageFactory mf = MessageFactory.newInstance();
-            SOAPMessage sm = mf.createMessage();
-
-            SOAPHeader sh = sm.getSOAPHeader();
-            SOAPBody sb = sm.getSOAPBody();
-            sh.detachNode();
-            QName bodyName = new QName("http://localhost:8090/services/ServiceConsumer", "ping", "ping");
-            SOAPBodyElement bodyElement = sb.addBodyElement(bodyName);
-            QName qn = new QName("aName");
-            SOAPElement quotation = bodyElement.addChildElement(qn);
-
-            quotation.addTextNode("TextMode");
-
-            System.out.println("\n Soap Request:\n");
-            sm.writeTo(System.out);
-            System.out.println();
-
-            URL endpoint = new URL("http://yourServer.com");
-            SOAPMessage response = connection.call(sm, endpoint);
-            System.out.println(response.getContentDescription());
+        try { // Call Web Service Operation
+          compositeapp1.ProtoWebService port = service.getCasaPort1();
+            // TODO initialize WS operation arguments here
+            java.lang.String ping = "ping";
+            // TODO process result here
+            java.lang.String result = port.ping(ping);
+            System.out.println("Result = "+result);
         } catch (Exception ex) {
-            ex.printStackTrace();
+            // TODO handle custom exceptions here
         }
 
-        try {
-            // Create SOAP Connection
-            SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
-            SOAPConnection soapConnection = soapConnectionFactory.createConnection();
-
-            // Send SOAP Message to SOAP Server
-            String url = "http://ws.cdyne.com/emailverify/Emailvernotestemail.asmx";
-            SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(), url);
-
-            // Process the SOAP Response
-            printSOAPResponse(soapResponse);
-
-            soapConnection.close();
-        } catch (Exception e) {
-            System.err.println("Error occurred while sending SOAP Request to Server");
-            e.printStackTrace();
-        }
-
-        return "Hello " + txt + " !";
+        return null;
     }
 
-    private static SOAPMessage createSOAPRequest() throws Exception {
-        MessageFactory messageFactory = MessageFactory.newInstance();
-        SOAPMessage soapMessage = messageFactory.createMessage();
-        SOAPPart soapPart = soapMessage.getSOAPPart();
-
-        String serverURI = "http://ws.cdyne.com/";
-
-        // SOAP Envelope
-        SOAPEnvelope envelope = soapPart.getEnvelope();
-        envelope.addNamespaceDeclaration("example", serverURI);
-
-        /*
-         Constructed SOAP Request Message:
-         <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:example="http://ws.cdyne.com/">
-         <SOAP-ENV:Header/>
-         <SOAP-ENV:Body>
-         <example:VerifyEmail>
-         <example:email>mutantninja@gmail.com</example:email>
-         <example:LicenseKey>123</example:LicenseKey>
-         </example:VerifyEmail>
-         </SOAP-ENV:Body>
-         </SOAP-ENV:Envelope>
-         */
-        // SOAP Body
-        SOAPBody soapBody = envelope.getBody();
-        SOAPElement soapBodyElem = soapBody.addChildElement("VerifyEmail", "example");
-        SOAPElement soapBodyElem1 = soapBodyElem.addChildElement("email", "example");
-        soapBodyElem1.addTextNode("mutantninja@gmail.com");
-        SOAPElement soapBodyElem2 = soapBodyElem.addChildElement("LicenseKey", "example");
-        soapBodyElem2.addTextNode("123");
-
-        MimeHeaders headers = soapMessage.getMimeHeaders();
-        headers.addHeader("SOAPAction", serverURI + "VerifyEmail");
-
-        soapMessage.saveChanges();
-
-        /* Print the request message */
-        System.out.print("Request SOAP Message = ");
-        soapMessage.writeTo(System.out);
-        System.out.println();
-
-        return soapMessage;
-    }
-
-    /**
-     * Method used to print the SOAP Response
-     */
-    private static void printSOAPResponse(SOAPMessage soapResponse) throws Exception {
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory.newTransformer();
-        Source sourceContent = soapResponse.getSOAPPart().getContent();
-        System.out.print("\nResponse SOAP Message = ");
-        StreamResult result = new StreamResult(System.out);
-        transformer.transform(sourceContent, result);
-    }
 }
 
---
+++ Consumer/src/main/java/cons/Consumer1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cons;
+
+/**
+ *
+ * @author pdlsoa
+ */
+public class Consumer1 {
+
+    public static void main(String args[]) {
+
+
+        try { // Call Web Service Operation
+            compositeapp1.CompositeApp1Service1 service = new compositeapp1.CompositeApp1Service1();
+            compositeapp1.ProtoWebService port = service.getCasaPort1();
+            // TODO initialize WS operation arguments here
+            java.lang.String ping = "ping";
+            // TODO process result here
+            java.lang.String result = port.ping(ping);
+            System.out.println("Result = "+result);
+        } catch (Exception ex) {
+            // TODO handle custom exceptions here
+        }
+
+    }
+}
