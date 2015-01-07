8701e7fded6783d7481c9c2e5879631dd0e0feed
Nicolas Levaique <nicolals.levaique@gmail.com>
2015-01-07 15:01:48 +0100

    send result to elasticsearch

--- Orchestrator/src/main/java/com/insa/swim/orchestrator/Controller.java
+++ Orchestrator/src/main/java/com/insa/swim/orchestrator/Controller.java
@@ -1,47 +1,101 @@
 package com.insa.swim.orchestrator;
 
 import com.insa.swim.orchestrator.xml.IXmlReader;
+import com.insa.swim.orchestrator.xml.Result;
 import com.insa.swim.orchestrator.xml.Scenario;
-import com.insa.swim.orchestrator.xml.XmlParser;
+//import com.insa.swim.orchestrator.xml.XmlParser;
+import java.io.IOException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Controller {
-	
-	public static final Logger LOGGER = LogManager.getLogger(Controller.class);
-	
-	private Scenario parseXml() {
-		IXmlReader xmlReader = new XmlParser();
-		Scenario scenario = xmlReader.parseXml();
-		return scenario;
-	}
-	
-	private void configureWebServices(Scenario scenario) {
+
+    public static final Logger LOGGER = LogManager.getLogger(Controller.class);
+
+//	private Scenario parseXml() {
+//		IXmlReader xmlReader = new XmlParser();
+//		Scenario scenario = xmlReader.parseXml();
+//		return scenario;
+//	}
+    private void configureWebServices(Scenario scenario) {
 //		WebServicesConfiguration config = new WebServicesConfiguration();
 //		config.configure(scenario);
-	}
+    }
 
-	private void launchEsbTest() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	private Listener startListener() {
-		Listener listener = new Listener();
-		listener.start();
-		return listener;
-	}
+    private void launchEsbTest() {
+        // TODO Auto-generated method stub
+    }
 
-	public void start() {
-		LOGGER.trace("Controller starts");
-		
-		Scenario scenario = parseXml();
-		
-		configureWebServices(scenario);
-		
-		Listener listener = startListener();
-		launchEsbTest();
-		listener.stop();
-		
-	}
+    private Listener startListener() {
+        Listener listener = new Listener();
+        listener.start();
+        return listener;
+    }
+
+    public void start() {
+        LOGGER.trace("Controller starts");
+
+        //Scenario scenario = parseXml();
+
+        //configureWebServices(scenario);
+
+        Listener listener = startListener();
+        launchEsbTest();
+        listener.stop();
+
+        // creating the httpClient which will be used by the threads
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(100);
+
+        CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(connectionManager).build();
+
+        try {
+            
+            // create a thread for each URI
+            PostToElasticSearchThread[] threads = new PostToElasticSearchThread[10];
+            for (int i = 0; i < threads.length; i++) {
+                Result result = new Result();
+                result.setConsumer("consumer" + i);
+                result.setProvider("provider" + i);
+                result.setC2PTime(i*i*12);
+                result.setP2CTime(15478);
+                threads[i] = new PostToElasticSearchThread(httpClient, result);
+            }
+
+            // start the threads
+            for (int j = 0; j < threads.length; j++) {
+                threads[j].start();
+            }
+
+            // join the threads
+            for (int j = 0; j < threads.length; j++) {
+                try {
+                    threads[j].join();
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        } finally {
+            try {
+                httpClient.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+
+//        ResultHandler resultHandler = new ResultHandler();
+//        Result result = new Result();
+//        result.setConsumer("consumer2");
+//        result.setProvider("provider3");
+//        result.setC2PTime(251);
+//        result.setP2CTime(324);
+//        resultHandler.sendResultsToElasticsearch(result);
+
+    }
 }
---
+++ Orchestrator/src/main/java/com/insa/swim/orchestrator/PostToElasticSearchThread.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.insa.swim.orchestrator;
+
+import com.insa.swim.orchestrator.xml.Result;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author pdlsoa
+ */
+public class PostToElasticSearchThread extends Thread {
+
+    private final static Logger LOGGER = LogManager.getLogger(PostToElasticSearchThread.class);
+
+    private final static String ELASTICSEARCH_URL = "http://localhost:9200/";
+    private final static String RESULT_INDEX = "open_esb/";
+    private final static String RESULT_TYPE = "results/";
+
+    private HttpClient httpClient;
+    private Result result;
+
+    public PostToElasticSearchThread(HttpClient httpClient, Result result) {
+        this.httpClient = httpClient;
+        this.result = result;
+    }
+
+    public void run() {
+        try {
+            HttpPost postRequest = new HttpPost(ELASTICSEARCH_URL + RESULT_INDEX + RESULT_TYPE);
+            StringEntity input = new StringEntity(result.toString());
+            input.setContentType("application/json");
+            postRequest.setEntity(input);
+            HttpResponse response = httpClient.execute(postRequest);
+
+            if (response.getStatusLine().getStatusCode() != 201) {
+                throw new RuntimeException("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
+            }
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader((response.getEntity().getContent())));
+
+            String output;
+            LOGGER.debug("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                LOGGER.debug(output);
+            }
+
+        } catch (IOException ex) {
+            LOGGER.error("An error occured while sending result to ElasticSearch");
+            LOGGER.debug(ex.getStackTrace());
+        }
+    }
+}
