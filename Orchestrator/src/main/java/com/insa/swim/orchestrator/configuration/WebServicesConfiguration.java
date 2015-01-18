0486a697c98f0b3a22bcac7292a73ad96ead7d71
Alex Binguy <alex@binguy.com>
2015-01-18 15:26:05 +0100

    Add provider settings : time + size

--- Orchestrator/src/main/java/com/insa/swim/orchestrator/configuration/WebServicesConfiguration.java
+++ Orchestrator/src/main/java/com/insa/swim/orchestrator/configuration/WebServicesConfiguration.java
@@ -6,8 +6,6 @@ import com.insa.swim.orchestrator.amqp.AMQPHandler;
 import com.insa.swim.orchestrator.xml.Scenario;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -25,15 +23,17 @@ public class WebServicesConfiguration {
     public void configure(Scenario scenario) {
         ArrayList<String> list = scenarioToString(scenario);
         for (String s : list) {
-            System.out.println(s);
+            LOGGER.debug("Configuration : " + s );
         }
         try {
-            for (int i = 0; i < list.size(); i++) {
+            // XXX TEMPORARY DIRTY CODE
+            for (int i = 0; i < list.size() ; i++) {
+                //amqp.sendConf("C2", "ceci est un message");
                 amqp.sendConf("C" + (i+1), list.get(i));
                 LOGGER.debug("Configuration message sent to C" + (i+1) );
             }
         } catch (IOException ex) {
-            LOGGER.error(Controller.class.getName() + " " + ex.getMessage());
+            LOGGER.error(Controller.class.getName() + " " + ex.toString());
         }
     }
 
@@ -56,12 +56,14 @@ public class WebServicesConfiguration {
             buff.append(c.getName()).append("|");
             for (Scenario.Consumers.Consumer.Requests.Request r : c.getRequests().getRequest()) {
                 buff.append("REQUEST|");
-                buff.append(r.getProviderId()).append("|");
+                buff.append(r.getProviderSettings().getProviderId()).append("|");
                 buff.append(r.getSendingTime()).append("|");
                 buff.append(r.getSize()).append("|");
                 buff.append(r.getPeriodic()).append("|");
                 buff.append(r.getPeriod()).append("|");
                 buff.append(r.getNumberRequests()).append("|");
+                buff.append(r.getProviderSettings().getProcessingTime()).append("|");
+                buff.append(r.getProviderSettings().getResponseSize()).append("|");
             }
             result.add(c.getId() - 1, buffinf.toString() + buff.toString());
         }
