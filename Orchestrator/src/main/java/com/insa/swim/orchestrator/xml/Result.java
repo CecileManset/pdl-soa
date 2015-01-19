ee6405ed9350973f6770cb0ba972ea48f938e50d
Nicolas <nicolas.levaique@ubleam.com>
2015-01-19 22:45:57 +0100

    result constructor now parses the request

--- Orchestrator/src/main/java/com/insa/swim/orchestrator/xml/Result.java
+++ Orchestrator/src/main/java/com/insa/swim/orchestrator/xml/Result.java
@@ -24,25 +24,57 @@ public class Result {
 
     @XmlAttribute(name ="provider", required = true)
     private String provider;
+    
+    @XmlAttribute(name ="requestSize", required = true)
+    private int requestSize;
+    
+    @XmlAttribute(name ="responseSize", required = true)
+    private int responseSize;
+    
+    @XmlAttribute(name ="processingTime", required = true)
+    private int processingTime;
 
-    @XmlAttribute(name ="timestamp", required = true)
-     private int timestamp;
+    @XmlAttribute(name ="sendingDateConsumer", required = true)
+    private int sendingDateConsumer;
+    
+    @XmlAttribute(name ="receptionDateProvider", required = true)
+    private int receptionDateProvider;
+    
+    @XmlAttribute(name ="sendingDateProvider", required = true)
+    private int sendingDateProvider;
+    
+    @XmlAttribute(name ="receptionDateConsumer", required = true)
+    private int receptionDateConsumer;
 
     @XmlAttribute(name ="C2PTime", required = true)
-     private int C2PTime;
+    private int C2PTime;
 
     @XmlAttribute(name ="P2CTime", required = true)
     private int P2CTime;
 
     public Result(String resultString) {
-        int min = 1;
-        int max = 5;
-        int consumerId = min + (int) (Math.random() * (max - min) + 1);
-        this.consumer = "consumer" + consumerId;
-        int providerId = min + (int) (Math.random() * (max - min) + 1);
-        this.provider = "provider" + providerId;
-        this.C2PTime = providerId * consumerId * 12;
-        this.P2CTime = consumerId * 27;
+        resultString = "1|1|50|100|1000|1421678470|1421678570|1421678670|1421678770";
+//        int min = 1;
+//        int max = 5;
+//        int consumerId = min + (int) (Math.random() * (max - min) + 1);
+//        this.consumer = "consumer" + consumerId;
+//        int providerId = min + (int) (Math.random() * (max - min) + 1);
+//        this.provider = "provider" + providerId;
+//        this.C2PTime = providerId * consumerId * 12;
+//        this.P2CTime = consumerId * 27;
+        
+        String[] resultParsed = resultString.split("\\|");
+        this.consumer = resultParsed[0];
+        this.provider = resultParsed[1];
+        this.requestSize = Integer.parseInt(resultParsed[2]);
+        this.responseSize = Integer.parseInt(resultParsed[3]);
+        this.processingTime = Integer.parseInt(resultParsed[4]);
+        this.sendingDateConsumer = Integer.parseInt(resultParsed[5]);
+        this.receptionDateProvider = Integer.parseInt(resultParsed[6]);
+        this.sendingDateProvider = Integer.parseInt(resultParsed[7]);
+        this.receptionDateConsumer = Integer.parseInt(resultParsed[8]);
+        this.C2PTime = this.receptionDateProvider - this.sendingDateConsumer;
+        this.P2CTime = this.receptionDateConsumer - this.sendingDateProvider;
     }
 
     public int getC2PTime() {
@@ -61,8 +93,32 @@ public class Result {
         return provider;
     }
 
-    public int getTimestamp() {
-        return timestamp;
+    public int getRequestSize() {
+        return requestSize;
+    }
+
+    public int getResponseSize() {
+        return responseSize;
+    }
+
+    public int getProcessingTime() {
+        return processingTime;
+    }
+
+    public int getSendingDateConsumer() {
+        return sendingDateConsumer;
+    }
+
+    public int getReceptionDateProvider() {
+        return receptionDateProvider;
+    }
+
+    public int getSendingDateProvider() {
+        return sendingDateProvider;
+    }
+
+    public int getReceptionDateConsumer() {
+        return receptionDateConsumer;
     }
 
     public void setC2PTime(int C2PTime) {
@@ -81,20 +137,20 @@ public class Result {
         this.provider = provider;
     }
 
-    public void setTimestamp(int timestamp) {
-        this.timestamp = timestamp;
-    }
-
     public org.json.JSONObject toJSON() {
         org.json.JSONObject resultJSON = new org.json.JSONObject();
         resultJSON.put("consumer", consumer);
         resultJSON.put("provider", provider);
         resultJSON.put("C2PTime", C2PTime);
         resultJSON.put("P2CTime", P2CTime);
-        resultJSON.put("timestamp", timestamp);
+        resultJSON.put("sendingDateConsumer", sendingDateConsumer);
+        resultJSON.put("receptionDateProvider", receptionDateProvider);
+        resultJSON.put("sendingDateProvider", sendingDateProvider);
+        resultJSON.put("receptionDateConsumer", receptionDateConsumer);        
         return resultJSON;
     }
 
+    @Override
     public String toString() {
         return toJSON().toString();
     }
